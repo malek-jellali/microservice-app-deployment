5c59db056b414dd58905ab165296b07036f4ac07
malak <malek.jellali@etudiant-fst.utm.tn>
2024-11-10 11:57:56 +0100

    correct jenkinsfile and add setup file as a flag  mechanism

--- offers-microservice-spring-boot/src/test/java/com/microservices/offers/OffersApplicationTests.java
+++ offers-microservice-spring-boot/src/test/java/com/microservices/offers/OffersApplicationTests.java
@@ -2,8 +2,10 @@ package com.microservices.offers;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "eureka.client.enabled=false")
 class OffersApplicationTests {
 
 	@Test
