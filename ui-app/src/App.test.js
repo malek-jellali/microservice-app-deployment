ba75196cb4cbeb57eec7ad6a95bdae9cc1ceebd2
malak <malek.jellali@etudiant-fst.utm.tn>
2024-11-12 15:03:20 +0100

    add gitignore

--- ui-app/src/App.test.js
+++ ui-app/src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import '@testing-library/jest-dom';
 
-test('renders learn react link', () => {
+import App from './App';  // Make sure you import the App component correctly
+
+test('renders App component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Web App/i);  // Match the actual text in your App component
   expect(linkElement).toBeInTheDocument();
 });
