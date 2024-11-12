5a4430744a98b8a83155afcc6714579112b0e9a2
malak <malek.jellali@etudiant-fst.utm.tn>
2024-11-12 10:00:01 +0100

    add setup file

---
+++ wishlist-microservice-python/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wishlist_microservice",
+    version="0.1.0",
+    packages=find_packages(),  # Automatically finds packages in the directory
+)
